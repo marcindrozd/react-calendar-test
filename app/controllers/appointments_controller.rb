97dae53cc5f4181a471628841f50d9507a29ddb5
Marcin Drozd <marcindrozd@me.com>
2017-09-23 18:52:28 +0200

    Clean up the controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,26 +1,29 @@
 class AppointmentsController < ApplicationController
   def index
-    @appointments = Appointment.order('appt_time DESC')
-    @appointment = Appointment.new
+    appointments = Appointment.order('appt_time DESC')
 
     respond_to do |format|
       format.html
-      format.json { render json: @appointments }
+      format.json { render json: appointments }
     end
   end
 
   def show
-    @appointment = Appointment.find(params[:id])
-    render json: @appointment
+    appointment = Appointment.find(params[:id])
+
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: appointment }
+    end
   end
 
   def create
-    @appointment = Appointment.new(appointment_params)
+    appointment = Appointment.new(appointment_params)
 
-    if @appointment.save
-      render json: @appointment
+    if appointment.save
+      render json: appointment
     else
-      render json: @appointment.errors, status: :unprocessable_entity
+      render json: appointment.errors, status: :unprocessable_entity
     end
   end
 
@@ -29,18 +32,18 @@ class AppointmentsController < ApplicationController
   end
 
   def update
-    @appointment = Appointment.find(params[:id])
+    appointment = Appointment.find(params[:id])
 
-    if @appointment.update(appointment_params)
-      render json: @appointment
+    if appointment.update(appointment_params)
+      render json: appointment
     else
-      render json: @appointment.errors, status: :unprocessable_entity
+      render json: appointment.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @appointment = Appointment.find(params[:id])
-    @appointment.destroy
+    appointment = Appointment.find(params[:id])
+    appointment.destroy
 
     head :no_content, status: :ok
   end
